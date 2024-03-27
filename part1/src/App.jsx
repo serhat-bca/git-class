ab8eda33ed0b32f59b7683efdcc11cd92463241f
serhatprogramming <serhatprogramming@gmail.com>
2024-03-27 11:57:32 -0400

    init

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,17 +1,28 @@
+const Hello = (props)=>(
+  <div>
+    <p>Hello {props.name}. You are number {props.number}</p>
+  </div>
+)
+
 
 const App = () => {
   console.log("Hello World, from console")
   const now = new Date();
   const a = 10;
   const b = 20;
+  const name = 'Tim';
   console.log(now, a+b)
+  const friends = ['Peter', 'July'];
   return (
-    <div>
+    <>
+    <Hello name="George" number = {27}/>
+    <Hello name={name} number = {a+b} />
       <p>Hello World, it is {now.toString()} </p>
       <p>
         {a} plus {b} is {a+b}
       </p>
-    </div>
+      <p>{friends}</p>
+    </>
   )}
 
 export default App
